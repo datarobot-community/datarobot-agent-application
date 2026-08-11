4ff876fb690b5603a76291902e9f87655f7d27b4
Anatolii Stehnii <anatolii.stehnii@datarobot.com>
2026-08-11 12:17:35 +0300

    Restore uv build and start: https://github.com/datarobot/recipe-datarobot-agent-application/pull/576
    Fixes

--- fastapi_server/start-app.sh
+++ fastapi_server/start-app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python3 alembic_migration.py  # migrating base to the last change
-uvicorn app.main:app --host 0.0.0.0 --port 8080 --proxy-headers --timeout-keep-alive 300
+uv run python3 alembic_migration.py  # migrating base to the last change
+uv run uvicorn app.main:app --host 0.0.0.0 --port 8080 --proxy-headers --timeout-keep-alive 300
